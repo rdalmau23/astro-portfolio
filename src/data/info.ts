6aa889c408489ca6cf69f9b690b22f5ebb4edccb
rdalmau23 <rdalmau23@ilg.cat>
2024-11-12 16:43:57 +0100

    test

---
+++ src/data/info.ts
@@ -0,0 +1,73 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Lara Cuevas",
+  jobDescription: "Web Developer",
+  about: `(2003, Barcelona, Spain) soy una diseñadora y estilista. Graduada en ESDI by EATM en Diseño de Moda y Estilismo de Indumentaria en 2023. 
+  En mis trabajos busco reflejar mi estilo “romántico y soñador” contrapuesto con algo mas “trash y rebelde”.
+   Me gusta tratar y reflejar temas sociales desde otro punto de vista mas artístico y creativo e investigar sobre tribus urbanas o el arte en general.
+  `,
+
+  experience: [
+
+    {
+      name: "",
+      location: "",
+      startDate: "",
+      endDate: "",
+      description: [
+        "",
+        "",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "",
+      location: "",
+      startDate: "",
+      endDate: "",
+      description: [
+        "",
+      ],
+    },
+    
+  ],
+
+  socialMedia: {
+    twitter: "https://twitter.com/Uzzii_21",
+    email: "mailto:uzairahmed@74372gmail.com",
+    linkedin: "https://www.linkedin.com/in/uzzii21/",
+  },
+
+  projects: [
+    {
+      title: "01. Holy Spirit",
+      isFeatured: true,
+      thumbnails: [
+        "/assets/images/img_0927-1.webp",
+        "/assets/images/74b9d07b-4231-454d-b985-c3c59511e306-1.webp",
+        "/assets/images/img_0935-1.webp",
+        "/assets/images/img_0940-1.webp",
+        "/assets/images/img_0939-edited.webp"
+      ],
+      
+    },
+    {
+      title: "02. You're a bad girl...",
+      isFeatured: true,
+      thumbnails: [
+        "/assets/images/img_7384.webp",
+        "/assets/images/img_7361.webp",
+        "/assets/images/img_8473.webp",
+        "/assets/images/img_7395.webp",
+        "/assets/images/dsc_4174.webp",
+        "/assets/images/img_8472.webp",
+        "/assets/images/dsc_4169.webp",
+        "/assets/images/img_7415.webp"
+      ],
+      
+    },
+  ],
+  
+};
